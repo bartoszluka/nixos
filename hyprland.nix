6c7467f1172658f1e0bf16454b45402c9d8d07be
bartek <bartoszluka1@gmail.com>
2024-05-30 00:27:42 +0200

    Change font size

--- hyprland.nix
+++ hyprland.nix
@@ -9,13 +9,11 @@
 in {
   programs.waybar.enable = true;
 
-  gtk.enable = true;
-
   home.pointerCursor = {
     gtk.enable = true;
     # x11.enable = true;
     package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
+    name = "Bibata-Modern-Ice";
     size = 16;
   };
 
@@ -32,8 +30,8 @@ in {
     };
 
     font = {
-      name = "Rubik";
-      size = 14;
+      # name = "Rubik";
+      size = 12;
     };
   };
 
