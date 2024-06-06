8169e89c8d68b756850f470c49669292ca63f0d0
bartek <bartoszluka1@gmail.com>
2024-06-06 14:12:55 +0200

    Add firefox addons

--- firefox.nix
+++ firefox.nix
@@ -5,7 +5,7 @@
 }: {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox-devedition;
+    # package = pkgs.firefox-devedition;
     nativeMessagingHosts = [
       # Gnome shell native connector
       pkgs.gnome-browser-connector
@@ -14,11 +14,26 @@
     ];
 
     profiles.bartek = {
+      isDefault = true;
+      containersForce = true;
       extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
         bitwarden
         ublock-origin
         darkreader
         vimium
+        # vimium-c
+        tridactyl
+        consent-o-matic # Automatic handling of GDPR consent forms
+        # container-tabs-sidebar
+        # enhanced-github
+        # facebook-container
+        # firefox-translations
+        # floccus # Sync your bookmarks and tabs across browsers via Nextcloud, any WebDAV service, any Git service, via a local file, via Google Drive.
+        sidebartabs
+        libredirect
+        lovely-forks
+        refined-github
+        unwanted-twitch
       ];
 
       search.default = "searx";
@@ -79,6 +94,7 @@
       '';
 
       settings = {
+        "apz.doubletapzoom.defaultzoomin" = 1.2; # default zoom value
         "browser.aboutwelcome.enabled" = false;
         "browser.meta_refresh_when_inactive.disabled" = true;
         "browser.urlbar.suggest.quicksuggest.sponsored" = false;
@@ -93,7 +109,51 @@
         "browser.startup.homepage" = "https://searx.jaimo.fun";
 
         # taken from Misterio77's config
-        "browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","library-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":18,"newElementCount":4}'';
+        "browser.uiCustomization.state" = ''
+          {
+            "placements": {
+              "widget-overflow-fixed-list": [],
+              "nav-bar": [
+                "back-button",
+                "forward-button",
+                "stop-reload-button",
+                "home-button",
+                "urlbar-container",
+                "downloads-button",
+                "library-button",
+                "ublock0_raymondhill_net-browser-action",
+                "_testpilot-containers-browser-action"
+              ],
+              "toolbar-menubar": [
+                "menubar-items"
+              ],
+              "TabsToolbar": [
+                "tabbrowser-tabs",
+                "new-tab-button",
+                "alltabs-button"
+              ],
+              "PersonalToolbar": [
+                "import-button",
+                "personal-bookmarks"
+              ]
+            },
+            "seen": [
+              "save-to-pocket-button",
+              "developer-button",
+              "ublock0_raymondhill_net-browser-action",
+              "_testpilot-containers-browser-action"
+            ],
+            "dirtyAreaCache": [
+              "nav-bar",
+              "PersonalToolbar",
+              "toolbar-menubar",
+              "TabsToolbar",
+              "widget-overflow-fixed-list"
+            ],
+            "currentVersion": 18,
+            "newElementCount": 4
+          }
+        '';
         "dom.security.https_only_mode" = true;
         "identity.fxaccounts.enabled" = false;
         "privacy.trackingprotection.enabled" = true;
